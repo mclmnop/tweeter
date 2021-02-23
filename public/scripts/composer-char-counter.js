3068bb87be619c0b002df01796d1fec5f327e176
mclmnop <m_charlebois@hotmail.com>
2021-02-23 14:39:30 -0500

    counter implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  //get text ZoneInput
+  const textZone = $("form");
+  //const textZone = $(".textInput");
+  
+  //get the length of the input by finding the text box which is a child
+  textZone.on("keydown", function(event) {
+    const text = $(this).find('#tweet-text');
+    let charCount = 140 - text.val().length;
+
+    //set the good value to counter
+    let counter = $(this).find('.counter').val(charCount);
+    if (charCount < 0) {
+      counter.addClass("counterRed");
+    }
+    if (charCount > 0 && counter.hasClass("counterRed")) {
+      counter.removeClass("counterRed");
+    }
+ 
+  });
+
+
+  //get counter value
+  
+
+});
