30e01861114a979cb30b51fd3069483f2c81fd3f
mclmnop <m_charlebois@hotmail.com>
2021-02-24 15:19:44 -0500

    pulling tweets from fake db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,61 +4,22 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-/* const tweetData = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "/images/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-   //"created_at": 1614175767849
-  //  "created_at": 1614086671657
-// "created_at": 1461116232227
-  "created_at": 1611171915227
-}] */
-
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-] 
 
 // returns strings for when the post what made, 10 days ago, 10 months, etc
 const calculateTimeAgo = function(dateCreated) {
   const now = new Date(Date.now());
   //console.log(now, dateCreated.getDate())
   if (now.getMonth() === dateCreated.getMonth() && now.getDate() === dateCreated.getDate()){
-    return 'today';
+      return 'today';
   } else if (now.getMonth() === dateCreated.getMonth() && now.getDate() - dateCreated.getDate() === 1){
-    return 'yesterday';
+      return 'yesterday';
   } else if (now.getMonth() === dateCreated.getMonth()){
-    const difference = now.getDate() - dateCreated.getDate();
-    return (difference,' days ago');
+      const difference = now.getDate() - dateCreated.getDate();
+      return (`${difference} days ago`);
   } else if (now.getYear() === dateCreated.getYear()){
-    const differenceDays = now.getDate() - dateCreated.getDate();
-    const differenceMonths= now.getMonth() - dateCreated.getMonth();
-    return (`${differenceDays} days ${differenceMonths} month(s) ago`)
+      const differenceDays = now.getDate() - dateCreated.getDate();
+      const differenceMonths = now.getMonth() - dateCreated.getMonth();
+      return (`${differenceDays} days ${differenceMonths} month(s) ago`);
   } else {
     return 'too long ago';
   }
@@ -85,12 +46,10 @@ $(document).ready(function() {
     </footer>
     </article>
     <br><br>
-    `;
+    `
     return tweetArticle;
   }
-  // const $tweet = createTweetElement(tweetData);
-  // console.log("tweeeet", $tweet);
-  // $('.tweets').append($tweet);
+
   const renderTweets = function(tweetArray) {
     //console.log(tweetArray);
     for (let tweet of tweetArray) {
@@ -99,9 +58,9 @@ $(document).ready(function() {
       $('.tweets').append(renderedTweet);
     }
   }
-  renderTweets(tweetData);
+  //renderTweets(tweetData);
 
-
+  //event listener for new tweets, posts result when submit
   $('form').on('submit', function(event) {
     // prevent the default behavior of the form submission
     event.preventDefault();
@@ -118,8 +77,20 @@ $(document).ready(function() {
       .done((result) => {
         console.log('result',result);
       })
-      .fail((err) => console.log('hein?', err.message));
-
+      .fail((err) => console.log(err.message));
   })
+
+  const loadTweets = function() {
+    $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+    })
+      .done((result) => {
+        //console.log('result',result);
+        renderTweets(result)
+      })
+      .fail((err) => console.log(err.message));
+  }
+  loadTweets();
 
 });
